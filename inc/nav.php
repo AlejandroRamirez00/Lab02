c8f83f98fe02bc3add32287fa9dcfde17d76c693
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-07-01 23:37:19 -0400

    fixed nav.php

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 <nav class = "menu">
 
     <ul>
-        <li><a href="siliconvalley.php"></a>Silicon Valley</li>
-        <li><a href="thylacine.php"></a>Thylacine</li>
+        <li><a href="siliconvalley.php">Silicon Valley</a></li>
+        <li><a href="thylacine.php">Thylacine</a></li>
     </ul>
 
 </nav>
